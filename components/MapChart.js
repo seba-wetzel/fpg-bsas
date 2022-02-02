0c4e00061915a185e453b0f048d87824fa7ec0dd
seba-wetzel <seba.wetzel@gmail.com>
2022-02-02 01:09:18 -0300

    SimpleMaps con la provincia de Buenos Aires y su division politica

---
+++ components/MapChart.js
@@ -0,0 +1,67 @@
+import { memo } from "react";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography
+} from "react-simple-maps";
+
+const geoUrl =
+  "/topo.toposjon";
+
+const rounded = num => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
+
+const MapChart = ({ setTooltipContent }) => {
+  return (
+    <>
+      <ComposableMap data-tip="" projectionConfig=""      projectionConfig={{
+        rotate: [60, 38, -1],
+        scale: 3000
+      }}>
+        <ZoomableGroup>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME, POP_EST } = geo.properties;
+                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
+  );
+};
+
+export default memo(MapChart);
