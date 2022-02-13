e7e497016f41a1c2940d7a3c17a6e1b25b1835fb
seba-wetzel <seba.wetzel@gmail.com>
2022-02-13 02:44:37 -0300

    Herramientas para agregar la seccion electoral al mapa y sus colores

---
+++ utils/transformTOPO.js
@@ -0,0 +1,20 @@
+import {distritos} from './seccionesElectorales.js'
+
+import * as fs from 'fs';
+console.log(distritos)
+const finder = distrito =>{
+    const pairs = Object.entries(distritos);
+    const toUpperCase = pairs.map(pair=> [pair[0], pair[1].map(i=> i.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g,""))])
+    const finded = toUpperCase.map(seccion=>seccion[1].includes(distrito)? seccion[0]: null).filter(i=>i).join('')
+    return finded
+}
+
+let rawdata = fs.readFileSync('../public/departamentos-buenos_aires.topojson');
+let mapa = JSON.parse(rawdata);
+const parsedDistritos = mapa.objects['departamentos-buenos_aires'].geometries.map(i=>({...i, properties:{...i.properties, seccion_electoral: finder(i.properties.departamento)}}));
+mapa.objects['departamentos-buenos_aires'].geometries = parsedDistritos;
+fs.writeFileSync('mapa-con-secciones-electorales.topojson', JSON.stringify(mapa));
+
+// process.argv.slice(2).forEach((val, index) => {
+//     console.log(`${index}: ${val}`)
+//   })
